b1bc263800bbcf4d5eb14d010ee134ea17c3b66b
Nitin-440 <56361554+Nitin-440@users.noreply.github.com>
2025-12-24 22:35:21 +0530

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,5 +1,5 @@
 import express from "express";
-import User from "../models/User.js";
+import User from "../models/user.js";
 import bcrypt from "bcrypt";
 import crypto from "crypto";
 import nodemailer from "nodemailer";
