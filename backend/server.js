118ffbac7f746fa17cab52e3f978a4e846d29589
avtar312 <87134632+avtar312@users.noreply.github.com>
2025-12-24 22:44:33 +0530

    Fix Render port

--- backend/server.js
+++ backend/server.js
@@ -39,4 +39,9 @@ app.get("*", (_, res) => {
   res.sendFile(path.join(frontendPath, "index.html"));
 });
 
-app.listen(5000, () => console.log("Server running at http://localhost:5000"));
+/* ================= START SERVER ================= */
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
