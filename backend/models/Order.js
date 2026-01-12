7bd5e78fccadb80616e3481f56a927efc0e3721b
chaunamhoang1-eng <chaunamhoang1@gmail.com>
2026-01-12 21:51:16 +0530

    Update Order.js

--- backend/models/Order.js
+++ backend/models/Order.js
@@ -1,5 +1,4 @@
 import mongoose from "mongoose";
-import { sendOrderToDiscord } from "../utils/discordWebhook.js";
 
 const orderSchema = new mongoose.Schema(
   {
@@ -74,12 +73,5 @@ orderSchema.index(
   { email: 1, fileURL: 1 },
   { unique: true }
 );
-
-/* 🔔 SEND NEW ORDER TO DISCORD */
-orderSchema.post("save", function (doc) {
-  if (doc.isNew) {
-    sendOrderToDiscord(doc);
-  }
-});
 
 export default mongoose.model("Order", orderSchema);
