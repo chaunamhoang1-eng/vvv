f593a98ad009a0c9958a0b5ecb40474d3e643c72
chaunamhoang1-eng <chaunamhoang1@gmail.com>
2026-01-14 17:59:03 +0530

    Update firebaseAdmin.js

--- backend/utils/firebaseAdmin.js
+++ backend/utils/firebaseAdmin.js
@@ -1,13 +1,21 @@
 import admin from "firebase-admin";
+import fs from "fs";
 
 if (!admin.apps.length) {
+  const serviceAccountPath = process.env.FIREBASE_SERVICE_ACCOUNT;
+
+  if (!serviceAccountPath) {
+    throw new Error("FIREBASE_SERVICE_ACCOUNT env variable is missing");
+  }
+
+  const serviceAccount = JSON.parse(
+    fs.readFileSync(serviceAccountPath, "utf8")
+  );
+
   admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-    }),
+    credential: admin.credential.cert(serviceAccount),
   });
 }
 
 export default admin;
+
