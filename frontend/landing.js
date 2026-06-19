45d9fda0d3888dad3d85096164e4cb45079a3480
Nitin kumar <chaunamhoang1@gmail.com>
2026-06-19 22:52:24 +0530

    Update landing.js

--- frontend/landing.js
+++ frontend/landing.js
@@ -1,1 +1,25 @@
 
+let installPrompt;
+
+window.addEventListener("beforeinstallprompt", (e) => {
+ e.preventDefault();
+
+ installPrompt = e;
+
+ const btn =
+ document.getElementById("installBtn");
+
+ if (btn) {
+   btn.style.display = "block";
+ }
+});
+
+async function installApp() {
+ if (!installPrompt) return;
+
+ installPrompt.prompt();
+
+ await installPrompt.userChoice;
+
+ installPrompt = null;
+}
