4902e432a6fb0ca7740c3a116fae13b494816ef5
Nitin kumar <chaunamhoang1@gmail.com>
2026-06-19 22:41:19 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,11 +1,21 @@
-window.onload = () => {
-  const popup = document.getElementById("discord-popup");
-  popup.style.visibility = "visible";
-  popup.style.opacity = "1";
-};
+let installPrompt;
 
-function closePopup() {
-  const popup = document.getElementById("discord-popup");
-  popup.style.opacity = "0";
-  setTimeout(() => popup.style.visibility = "hidden", 300);
+window.addEventListener("beforeinstallprompt", (e) => {
+ e.preventDefault();
+
+ installPrompt = e;
+
+ document
+  .getElementById("installBtn")
+  .style.display = "block";
+});
+
+async function installApp() {
+ if (!installPrompt) return;
+
+ installPrompt.prompt();
+
+ await installPrompt.userChoice;
+
+ installPrompt = null;
 }
