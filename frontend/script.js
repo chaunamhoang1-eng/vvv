585737597163cf354879aeee79ea6757e11458d4
Nitin kumar <chaunamhoang1@gmail.com>
2026-06-20 00:49:51 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,47 +1,27 @@
-let deferredPrompt = null;
-
 window.addEventListener(
 "beforeinstallprompt",
 (e) => {
+
 e.preventDefault();
 
-```
-deferredPrompt = e;
-
-setTimeout(() => {
-
-  if (!deferredPrompt) return;
-
-  deferredPrompt.prompt();
-
-  deferredPrompt.userChoice
-  .then((choice) => {
-
-    console.log(
-      "Install:",
-      choice.outcome
-    );
-
-    deferredPrompt = null;
-  });
-
-}, 3000);
-```
-
-}
+console.log(
+"INSTALL EVENT FIRED"
 );
+
+alert(
+"Install available"
+);
+
+e.prompt();
+
+});
 
 window.addEventListener(
 "appinstalled",
 () => {
 
-```
 alert(
-  "PlagX installed successfully!"
+"Installed"
 );
 
-deferredPrompt = null;
-```
-
-}
-);
+});
