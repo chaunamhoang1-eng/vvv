5db1f003ac2d3c07a9e46d67bce253aea34e8ec4
Nitin kumar <chaunamhoang1@gmail.com>
2026-08-21 16:45:31 +0530

    Update service-worker.js

--- frontend/service-worker.js
+++ frontend/service-worker.js
@@ -1,11 +1,20 @@
 self.addEventListener("install", () => {
- self.skipWaiting();
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
- event.waitUntil(clients.claim());
+  event.waitUntil(clients.claim());
 });
 
 self.addEventListener("fetch", (event) => {
- event.respondWith(fetch(event.request));
+  const url = new URL(event.request.url);
+
+  // Only intercept requests to your own website
+  if (url.origin !== self.location.origin) {
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request)
+  );
 });
